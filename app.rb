a4b009f8733e95a1334787d6f003c86c869f11d0
Todd Fisher <todd.fisher@gmail.com>
2013-05-28 15:39:37 -0400

    some refactoring for hopefully easier to follow code

--- app.rb
+++ app.rb
@@ -1,8 +1,7 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'openssl'
 require 'base64'
 require 'json'
-require 'thread'
 require 'curb'
 require 'uri'
 
@@ -14,24 +13,12 @@ k_clickid
 
 # kenshoo: https://148.xg4ken.com/media/redir.php?track=1&token=4cc651ab-d9bb-4c1f-a47b-8cfa5df6d150&type=call&val=24.60&orderId=&promoCode=&valueCurrency=USD&GCID=&kw=&product=
 =end
-post '/process' do
-  request_sig = request.env['HTTP_X_CTM_SIGNATURE']
-  request_time = request.env['HTTP_X_CTM_TIME']
-  puts "sig: #{request_sig}, time: #{request_time}"
-  digest = OpenSSL::Digest::Digest.new('sha1')
-  post_data = request.body.read#env["rack.input"].read
-  verify = Base64.encode64(OpenSSL::HMAC.digest(digest, ENV['CTM_SECRET'], request_time + post_data)).strip
-  if verify != request_sig
-    puts "request invalid"
-    return [403, {'Content-Type' => 'text/json'}, {'error' => 'invalid request'}.to_json]
-  end
-  puts "request valid"
-#  t = Thread.new do
-    puts "parse call"
-    call = JSON.parse(post_data)
+class KenshooApp < Sinatra::Base
+
+  def process_call(call)
     if !call['location'] || !call['location'].match(/k_clickid=/) # nothing to do here...
       puts "no tracking id: #{call['location'].inspect}"
-      return
+      return [201, {'Content-Type' => 'text/json'}, nil] # really a noop
     end
     puts "parsed call: #{call.inspect}"
     uri = URI.parse(call['location'])
@@ -46,6 +33,29 @@ post '/process' do
     r = Curl.get(url)
     puts r.header_str
     puts r.body_str
-#  end
-  [200, {'Content-Type' => 'text/json'}, post_data]
+    [201, {'Content-Type' => 'text/json'}, nil]
+  end
+
+  def verify_request(request)
+    request_sig = request.env['HTTP_X_CTM_SIGNATURE']
+    request_time = request.env['HTTP_X_CTM_TIME']
+    puts "sig: #{request_sig}, time: #{request_time}"
+    digest = OpenSSL::Digest::Digest.new('sha1')
+    post_data = request.body.read#env["rack.input"].read
+    verify = Base64.encode64(OpenSSL::HMAC.digest(digest, ENV['CTM_SECRET'], request_time + post_data)).strip
+    if verify != request_sig
+      return nil
+    end
+    JSON.parse(post_data)
+  end
+
+  post '/process' do
+    call = verify_request(request)
+    return [403, {'Content-Type' => 'text/json'}, {'error' => 'invalid request'}.to_json] if call.nil?
+
+    process_call(call)
+
+  end
+
+  run! if app_file == $0
 end
